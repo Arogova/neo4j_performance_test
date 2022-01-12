04c5f3fd106f3cc757c76e479ea495b93ef03948
Alexandra Rogova <rogova@irif.fr>
2022-01-12 12:59:58 +0100

    Apply canonical format to code

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/Arogova/neo4j_performance_test
 go 1.13
 
 require (
-	github.com/Arogova/neo4j_performance_test/utils v0.0.0-00010101000000-000000000000 // indirect
-	github.com/neo4j/neo4j-go-driver/v4 v4.4.0 // indirect
+	github.com/Arogova/neo4j_performance_test/utils v0.0.0-00010101000000-000000000000
+	github.com/neo4j/neo4j-go-driver/v4 v4.4.0
 )
 
 replace github.com/Arogova/neo4j_performance_test/utils => ./utils
--- utils/utils.go
+++ utils/utils.go
@@ -1,41 +1,41 @@
 package utils
 
 import (
-  "math/rand"
-  "fmt"
-  "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Returns a neo4j query that creates a random graph of n nodes such that
 // each pair of nodes is linked with probability p
 func CreateRandomGraphScript(n int, p float64) string {
-  rand.Seed(time.Now().UTC().UnixNano())
-  query := ""
-  for  i:=0; i<n; i++ {
-    query += fmt.Sprintf("CREATE (v%d {name:%d})\n", i, i)
-  }
-  for i:=0; i<n; i++ {
-    for j:=0; j<n; j++ {
-      if rand.Float64() <= p {
-        query += fmt.Sprintf("CREATE (v%d)-[:Edge]->(v%d)\n", i, j)
-      }
-    }
-  }
-  return query
+	rand.Seed(time.Now().UTC().UnixNano())
+	query := ""
+	for i := 0; i < n; i++ {
+		query += fmt.Sprintf("CREATE (v%d {name:%d})\n", i, i)
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if rand.Float64() <= p {
+				query += fmt.Sprintf("CREATE (v%d)-[:Edge]->(v%d)\n", i, j)
+			}
+		}
+	}
+	return query
 }
 
 // Returns a neo4j query that searches for two disjoint paths between
 // two random pairs of nodes
-func RandomTwoDisjointPathQuery (n int) string {
-  rand.Seed(time.Now().UTC().UnixNano())
-  return fmt.Sprintf(`MATCH p1 = (s1 {name: %d})-[:Edge*]-(t1 {name: %d})
+func RandomTwoDisjointPathQuery(n int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return fmt.Sprintf(`MATCH p1 = (s1 {name: %d})-[:Edge*]-(t1 {name: %d})
     MATCH p2 = (s2 {name: %d})-[:Edge*]-(t2 {name: %d})
     WHERE none(r in relationships(p2) WHERE r in relationships(p1))
     RETURN p1, p2`, rand.Intn(n), rand.Intn(n), rand.Intn(n), rand.Intn(n))
 }
 
-func HamiltonianPath () string {
-  return `MATCH (n)
+func HamiltonianPath() string {
+	return `MATCH (n)
   WITH collect(n.name) AS allNodes
   MATCH path=(s)-[:Edge*]-(t)
   WITH path, allNodes, [y in nodes(path) | y.name] as nodesInPath
