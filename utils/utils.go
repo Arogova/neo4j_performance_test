b8745d81ad82b2c273b066e7d43c1f124cb23ea4
Alexandra Rogova <rogova@irif.fr>
2022-01-12 12:56:31 +0100

    Make random a bit less deterministic

--- utils/utils.go
+++ utils/utils.go
@@ -3,11 +3,13 @@ package utils
 import (
   "math/rand"
   "fmt"
+  "time"
 )
 
 // Returns a neo4j query that creates a random graph of n nodes such that
 // each pair of nodes is linked with probability p
 func CreateRandomGraphScript(n int, p float64) string {
+  rand.Seed(time.Now().UTC().UnixNano())
   query := ""
   for  i:=0; i<n; i++ {
     query += fmt.Sprintf("CREATE (v%d {name:%d})\n", i, i)
@@ -25,7 +27,8 @@ func CreateRandomGraphScript(n int, p float64) string {
 // Returns a neo4j query that searches for two disjoint paths between
 // two random pairs of nodes
 func RandomTwoDisjointPathQuery (n int) string {
-    return fmt.Sprintf(`MATCH p1 = (s1 {name: %d})-[:Edge*]-(t1 {name: %d})
+  rand.Seed(time.Now().UTC().UnixNano())
+  return fmt.Sprintf(`MATCH p1 = (s1 {name: %d})-[:Edge*]-(t1 {name: %d})
     MATCH p2 = (s2 {name: %d})-[:Edge*]-(t2 {name: %d})
     WHERE none(r in relationships(p2) WHERE r in relationships(p1))
     RETURN p1, p2`, rand.Intn(n), rand.Intn(n), rand.Intn(n), rand.Intn(n))
