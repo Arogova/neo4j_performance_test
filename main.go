318238f7c76e76dd1ddd188c63f2e95c86fed57a
Alexandra Rogova <rogova@irif.fr>
2022-02-22 18:26:29 +0100

    Streamline result logging

--- main.go
+++ main.go
@@ -8,8 +8,17 @@ import (
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 	"math/rand"
 	"os"
+	"strconv"
 	"time"
 )
+
+type testResult struct {
+	nodes       int
+	probability float64
+	timeSpent   int
+	graph       string
+	query       string
+}
 
 func checkErr(err error) {
 	if err != nil {
@@ -17,18 +26,22 @@ func checkErr(err error) {
 	}
 }
 
-func writeDump(dumpFile *os.File, n int, p float64, timeSpent int, res int, graph string, query string) {
-	_, err := dumpFile.WriteString(fmt.Sprintf("%d,%f,%s,%d\n", n, p, timeSpent, res))
+func writeToFile(fileLocation *os.File, data *testResult) {
+	timeLayout := "15:04:05"
+	timeSpent := strconv.Itoa(data.timeSpent)
+	if timeSpent == "-1" {
+		timeSpent = "timeout"
+	}
+	toWrite := fmt.Sprintf("%d,%f,%s,%s\n", data.nodes, data.probability, timeSpent, time.Now().Format(timeLayout))
+	_, err := fileLocation.WriteString(toWrite)
 	checkErr(err)
-	_, err = dumpFile.WriteString(graph)
-	checkErr(err)
-	_, err = dumpFile.WriteString("\n")
-	checkErr(err)
-	_, err = dumpFile.WriteString(query)
-	checkErr(err)
-	_, err = dumpFile.WriteString("\n------\n")
-	checkErr(err)
+	if timeSpent == "timeout" {
+		toWrite = fmt.Sprintf("%s\n%s\n------\n", data.graph, data.query)
+		_, err = fileLocation.WriteString(toWrite)
+		checkErr(err)
+	}
 }
+
 
 // Executes the query given as argument
 // Sends number of results to channel c
@@ -38,14 +51,8 @@ func executeQuery(driver neo4j.Driver, queryString string, resChan chan int) {
 	_, err := session.ReadTransaction(func(tx neo4j.Transaction) (interface{}, error) {
 		result, err := tx.Run(queryString, nil)
 		checkErr(err)
-		result_count := 0
-		for result.Next() {
-			result.Record()
-			result_count += 1
-		}
 		summary, err := result.Consume()
 		checkErr(err)
-		resChan <- result_count
 		resChan <- int(summary.ResultAvailableAfter().Milliseconds())
 		return 1, err
 	})
@@ -64,14 +71,20 @@ func createRandomGraph(driver neo4j.Driver, graphString string) {
 	})
 }
 
-func testSuite(driver neo4j.Driver, queryType string, maxNodes int) {
+func createFiles() (*os.File, *os.File) {
 	timeLayout := "2006-02-01--15:04:05"
 	resultFile, err := os.Create("results/" + time.Now().Format(timeLayout) + ".csv")
 	checkErr(err)
+	_, err = resultFile.WriteString("order,edge probability,query execution time,timestamp\n")
 	dumpFile, err := os.Create("results/" + time.Now().Format(timeLayout) + "_dump.csv")
 	checkErr(err)
+	return resultFile, dumpFile
+}
+
+func testSuite(driver neo4j.Driver, queryType string, maxNodes int) {
+	resultFile, dumpFile := createFiles()
 	defer resultFile.Close()
-	_, err = resultFile.WriteString("order,edge probability,query execution time,number of results\n")
+	defer dumpFile.Close()
 	for p := 0.1; p < 1; p += 0.1 {
 		for n := 10; n <= maxNodes; n += 10 {
 			graph := utils.CreateRandomGraphScript(n, p)
@@ -87,23 +100,19 @@ func testSuite(driver neo4j.Driver, queryType string, maxNodes int) {
 					query = utils.HamiltonianPath()
 				}
 				go executeQuery(driver, query, c)
-				//start_time := time.Now()
 				select {
 				case res := <-c:
-					timeSpent := <-c
-					if res >= 500 {
-						writeDump(dumpFile, n, p, timeSpent, res, graph, query)
-					}
 					if !ignore {
-						_, err := resultFile.WriteString(fmt.Sprintf("%d,%f,%d,%d\n", n, p, timeSpent, res))
-						checkErr(err)
+						data := testResult{nodes: n, probability: p, timeSpent: res, graph: "", query: ""}
+						writeToFile(resultFile, &data)
 					}
 				case <-time.After(300 * time.Second):
 					if !ignore {
-						_, err := resultFile.WriteString(fmt.Sprintf("%d,%f,timeout,0\n", n, p))
-						checkErr(err)
+						data := testResult{nodes: n, probability: p, timeSpent: -1, graph: "", query: ""}
+						writeToFile(resultFile, &data)
 					}
-					writeDump(dumpFile, n, p, -1, 0, graph, query)
+					data := testResult{nodes: n, probability: p, timeSpent: -1, graph: graph, query: query}
+					writeToFile(dumpFile, &data)
 				}
 				ignore = false
 			}
