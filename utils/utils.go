8a66f857bf12102c7d9903a4739b25b228e1483c
Alexandra Rogova <rogova@irif.fr>
2022-11-14 17:40:26 +0100

    Add a*b* normal and automata tests + labeled graph creation script

--- utils/utils.go
+++ utils/utils.go
@@ -23,6 +23,26 @@ func CreateRandomGraphScript(n int, p float64) []string {
 	return query
 }
 
+func CreateLabeledGraphScript() []string{
+	query := make([]string, 0)
+	for i := 0; i < n; i++ {
+		query = append(query, fmt.Sprintf("CREATE ({name:%d})", i))
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			label := "a"
+			if rand.Float64() < 0.5{
+				label := "b"
+			}
+			if rand.Float64() <= p {
+				edgeQuery := fmt.Sprintf("MATCH (v1{name:%d}) MATCH (v2{name:%d}) CREATE (v1)-[:%v]->(v2)", i, j, label)
+				query = append(query, edgeQuery)
+			}
+		}
+	}
+	return query
+}
+
 func RandomTwoDisjointPathQuery(n int) string {
 	return fmt.Sprintf(`MATCH p1 = (s1 {name: %d})-[:Edge*]-(t1 {name: %d})
     MATCH p2 = (s2 {name: %d})-[:Edge*]-(t2 {name: %d})
@@ -63,4 +83,33 @@ func EulerianTrail() string {
 	WHERE all(edge in allEdges where edge in edgesInPath)
 	AND size(allEdges) = size(edgesInPath)
 	return path LIMIT 1`
+}
+
+func NormalABStart() {
+	return `MATCH p = ()-[:a *]-()-[:b *]-() RETURN p`
+}
+
+func AutomataABStart() {
+	return `MATCH p = (x)-[*]-(y)
+	WITH [r in relationships(p) | type(r)] as types_p, p
+	WITH reduce (state = 'q0', label in types_p |
+		CASE state
+			WHEN 'q0' THEN
+				CASE label
+					WHEN 'a' THEN 'q1'
+					WHEN 'b' THEN 'q2'
+					ELSE 'qs'
+				END
+			WHEN 'q2' THEN
+				CASE label
+					WHEN 'a' THEN 'qs'
+					WHEN 'b' THEN 'q2'
+					ELSE 'qs'
+				END
+			ELSE 'qs'
+		END
+	) as final_state, p
+	WHERE final_state in ['q2', 'q1']
+	RETURN p
+	`
 }
